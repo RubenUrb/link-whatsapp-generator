76a1f5766a2472ceb303667c3652b0e9a805dc48
Ruben Urbano Colmenar <ruben.urbano@vasscompany.com>
2025-02-16 22:22:43 +0100

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'link-whatsapp-generator';
+  cards: any[] = [];
+  card: any;
+  number: any;
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('/assets/data-structure.json').subscribe((data: any) => {
+      this.cards = data.cards;
+    });
+  }
+
+  generateLink() {
+    if (!this.number || !this.card) {
+      return;
+    }
+
+    const baseUrl = 'https://wa.me/34';
+    const message = `Hola, aquí tienes tu territorio asignado. 📍 Mapa: ${this.card.description}`;
+    const encodedMessage = encodeURIComponent(message);
+    const whatsappLink = `${baseUrl}${this.number}?text=${encodedMessage}`;
+    
+    window.open(whatsappLink, '_blank');
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
